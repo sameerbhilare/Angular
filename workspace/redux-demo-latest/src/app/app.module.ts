9fe0fdffe6c89ac9d511ec9603bc2aedda3990b0
Sameer Bhilare <bhilare.sameer@gmail.com>
2020-10-06 19:38:38 +0530

    enabling Redux Devtools

--- workspace/redux-demo-latest/src/app/app.module.ts
+++ workspace/redux-demo-latest/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { NgReduxModule, NgRedux } from '@angular-redux/store'; // imports
+import { NgModule, isDevMode } from '@angular/core';
+import {
+  NgReduxModule,
+  NgRedux,
+  DevToolsExtension,
+} from '@angular-redux/store'; // imports
 import { fromJS, Map } from 'immutable';
 
 import { AppComponent } from './app.component';
@@ -13,9 +17,20 @@ import { IAppState, INITIAL_STATE, rootReducer } from './store';
   bootstrap: [AppComponent],
 })
 export class AppModule {
-  constructor(ngRedux: NgRedux<Map<string, any>>) {
+  constructor(ngRedux: NgRedux<Map<string, any>>, devTools: DevToolsExtension) {
     // configuring the store with root app reducer and initial state (empty JS object)
     // fromJS() function gets a plain javascript object and returns an immutable object which is of type ap.
-    ngRedux.configureStore(rootReducer, fromJS(INITIAL_STATE));
+
+    // 3rd arg is to add middleware.
+    // middleware is an extension point.
+    // So if we can execute some code from the moment an action is dispatched
+    // to the moment it reaches a reducer.
+    // E.g. logging. we can log every action and do something about it.
+
+    // 4th arg is an array of enhancers, this is where we can use tools extension.
+    // this can cause performance issues. So make sure you use only in Devlopment mode.
+
+    var enhancers = isDevMode() ? [devTools.enhancer()] : [];
+    ngRedux.configureStore(rootReducer, fromJS(INITIAL_STATE), [], enhancers);
   }
 }
